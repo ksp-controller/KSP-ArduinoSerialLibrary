00640b709459df07c36f462d6f4357a3a63bfef5
Gabriel Debes <gabriel.wady@gmail.com>
2019-04-13 16:55:13 -0700

    let main be just an example

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,31 @@
-#include <Arduino.h>
-#include "./serialLib/SerialController.h"
-#include "./Potentiometer.h"
-static SerialController *serialCom;
-static Potentiometer *throttle;
-#define PIN1 20
-#define PIN2 21
+// #include <Arduino.h>
+// #include "./serialLib/SerialController.h"
+// #include "./Potentiometer.h"
+// static SerialController *serialCom;
+// static Potentiometer *throttle;
+// #define PIN1 20
+// #define PIN2 21
 
-void setup() {
-  serialCom = new SerialController();
-  pinMode(PIN1, OUTPUT);
-  pinMode(PIN2, OUTPUT);
-  throttle = new Potentiometer(A2,0,1000);
-}
-void loop() {
-  serialCom->loop();
-  if (GetControlStatus(serialCom->getVesselData(), RCS) == 1) {
-    digitalWrite(PIN1, HIGH);
-  }else {
-    digitalWrite(PIN1, LOW);
-  }
-  if (GetControlStatus(serialCom->getVesselData(), SAS) == 1) {
-    digitalWrite(PIN2, HIGH);
-  }else {
-    digitalWrite(PIN2, LOW);
-  }
-  throttle->loop();
-  SerializedVesselControls *control = new SerializedVesselControls();
-  control->Throttle = throttle->getCurrentValue();
-  serialCom->setVesselControls(*control);
-}
+// void setup() {
+//   serialCom = new SerialController();
+//   pinMode(PIN1, OUTPUT);
+//   pinMode(PIN2, OUTPUT);
+//   throttle = new Potentiometer(A2,0,1000);
+// }
+// void loop() {
+//   serialCom->loop();
+//   if (GetControlStatus(serialCom->getVesselData(), RCS) == 1) {
+//     digitalWrite(PIN1, HIGH);
+//   }else {
+//     digitalWrite(PIN1, LOW);
+//   }
+//   if (GetControlStatus(serialCom->getVesselData(), SAS) == 1) {
+//     digitalWrite(PIN2, HIGH);
+//   }else {
+//     digitalWrite(PIN2, LOW);
+//   }
+//   throttle->loop();
+//   SerializedVesselControls *control = new SerializedVesselControls();
+//   control->Throttle = throttle->getCurrentValue();
+//   serialCom->setVesselControls(*control);
+// }
